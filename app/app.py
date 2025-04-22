12b8242e5d1a0234c7592dd3dadca3cc7e229642
Chris Madsen <chris.madsen@gov.bc.ca>
2025-04-22 16:55:31 -0700

    made some inputs based on species names and populations

--- app/app.py
+++ app/app.py
@@ -20,43 +20,69 @@ app_ui = ui.page_fluid(
     ),
         ui.card(
         ui.h3("TOOLBOX"),
-        ui.ouput_ui("spec_sel"),
-        # ui.output_data_frame('rsm_dt'),
+        ui.input_select(id = "dom_sel", label = "Domain",choices=['Terrestrial','Aquatic'],selected=['Aquatic'],multiple=True),
+        ui.input_select(id = "spec_sel", label = "COSEWIC Common Name",choices=[],selected=[],multiple=True),
+        ui.input_select(id = "pop_sel", label = "Population",choices=[],selected=[],multiple=True),
+        ui.output_data_frame('rsm_dt'),
         {"class": "floating-toolbox"}
     )
 )
 
 def server(input, output, session):
     
+    bc_bound = gpd.read_file(Path(__file__).parent/"www\\bc_bound.gpkg")
     # sar_polys = gpd.read_file(Path(__file__).parent/"www\\dfo_sara_and_crit_hab_bulltrout_and_sockeye_data.gpkg")
-    
+    rsm = pd.read_csv(Path(__file__).parent/"www\\risk_status_merged.csv")
+        
+    # Update species that can be selected based on 
+    # Update species select input based on Chrissy's data table.
+    @reactive.effect
+    @reactive.event(input.dom_sel)
+    def _():
+        rsm_of_domain = rsm[rsm['Domain'].isin(input.dom_sel())]
+        rsm_of_domain = rsm_of_domain.sort_values(by='COSEWIC common name')
+        species_options = list(rsm_of_domain['COSEWIC common name'].unique())
+        ui.update_select(id='spec_sel',choices = species_options, selected=species_options[0])
+
     @reactive.calc
-    def rsm():
-        dat = pd.read_csv(Path(__file__).parent/"www\\risk_status_merged.csv")
-        return dat
+    def rsm_sp_f():
+        req(input.spec_sel())
+        return rsm[rsm['COSEWIC common name'].isin(input.spec_sel())]
     
-    # @render.ui
-    # def spec_sel():
-    #     rsm()['']
+    # Update populations that can be selected based on which species has been selected.
+    @reactive.effect
+    def _():
+        population_options = rsm_sp_f()['Legal population'].unique()
+        print(str(population_options))
+        if str(population_options) != '[nan]':   
+            population_options = population_options[~pd.isna(population_options)]
+            ui.update_select(id='pop_sel',choices = list(population_options), selected= population_options[0])
+            print('Updated population selection')
+        else:
+            ui.update_select(id='pop_sel',choices = [], selected=[])
+    
+    
     @render.data_frame
     def rsm_dt():
-       return render.DataTable(rsm(), selection_mode='row')
+       return render.DataTable(rsm, selection_mode='row')
     
-    # @render.ui
-    # def myleaf():
-    #     sar_polys_geojson = folium.GeoJson(data=sar_polys, style_function=lambda x: {
-    #         "fillColor": "orange",
-    #         "weight": 1,
-    #         "color": "black",
-    #         "fillOpacity": 0.75
-    #         })
-    #     my_map = folium.Map(data = [54.93147, -124.12823], zoom_start = 8)
-    #     sar_polys_geojson.add_to(my_map)
-    #     #boulder_coords = [40.015, -105.2705]
-    #     #Create the map
-    #     #my_map = folium.Map(location = boulder_coords, zoom_start = 13)
+    @render.ui
+    def myleaf():
+        # sar_polys_geojson = folium.GeoJson(data=sar_polys, style_function=lambda x: {
+        #     "fillColor": "orange",
+        #     "weight": 1,
+        #     "color": "black",
+        #     "fillOpacity": 0.75
+        #     })
+        bc = folium.GeoJson(data = bc_bound)
+        my_map = folium.Map(data = [54.93147, -124.12823], zoom_start = 15)
+        bc.add_to(my_map)
+        # sar_polys_geojson.add_to(my_map)
+        #boulder_coords = [40.015, -105.2705]
+        #Create the map
+        #my_map = folium.Map(location = boulder_coords, zoom_start = 13)
 
-    #     return my_map
+        return my_map
     
     #@reactive.effect
     #ui.update_selectize('spec_sel', choices = spdat()['species_names'])
